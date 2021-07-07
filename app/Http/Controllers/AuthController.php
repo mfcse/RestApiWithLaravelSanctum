a51706e24613f52feed998714bfe0fb75a09b9dc
Maruf <maruf.cu.cse09@gmail.com>
2021-07-07 19:32:29 +0600

    created rest api using sanctum auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $fields = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed|min:6'
+        ]);
+        $user = User::create([
+            'name' => $fields['name'],
+            'email' => $fields['email'],
+            'password' => bcrypt($fields['password'])
+        ]);
+        $token = $user->createToken('myAppToken')->plainTextToken;
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+        return response($response, 201);
+    }
+
+    public function login(Request $request)
+    {
+        $fields = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string|min:6'
+        ]);
+        $credentials = $request->except('_token');
+        $user = User::where('email', $fields['email'])->first();
+        if (!auth()->attempt($credentials)) {
+            //if (!$user || !Hash::check($fields['password'], $user->password)) {
+            return response([
+                'message' => 'Bad Login attempt'
+            ], 401);
+        }
+        $token = $user->createToken('myAppToken')->plainTextToken;
+        $response = [
+            'message' => 'Logged In',
+            'user' => $user,
+            'token' => $token
+        ];
+        return response($response, 201);
+    }
+
+    public function logout()
+    {
+        auth()->user()->tokens()->delete();
+        return [
+            'message' => 'Logged Out'
+        ];
+    }
+}
